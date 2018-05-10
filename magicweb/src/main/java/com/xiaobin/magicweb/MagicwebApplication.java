d7c635c12c62a8cbcefcecdc8b49f6c50915d576
lixiaobin_java <lixiaobin_java@163.com>
2018-05-10 17:18:39 +0800

    初始化

--- magicweb/src/main/java/com/xiaobin/magicweb/MagicwebApplication.java
+++ magicweb/src/main/java/com/xiaobin/magicweb/MagicwebApplication.java
@@ -1,9 +1,11 @@
 package com.xiaobin.magicweb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xiaobin.magicweb.mapper")//将项目中对应的mapper类的路径加进来就可以了
 public class MagicwebApplication {
 
     public static void main(String[] args) {
